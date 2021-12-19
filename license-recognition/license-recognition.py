018f637bc73bd9a0bab87ce16340bd87499d7856
Budi Kurniawan <budi2020@gmail.com>
2021-12-19 23:19:00 +1100

    Add output

--- license-recognition/license-recognition.py
+++ license-recognition/license-recognition.py
@@ -8,29 +8,29 @@ except ImportError:
     import Image
 import pytesseract
 
-img = cv2.imread('license-recognition/data/rabo-license-dataset/scaled/budi.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def resize(img):
+    scale_percent = 50 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-# If tesseract is not in PATH, include this:
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+def main():
+    input_path = 'license-recognition/data/rabo-license-dataset/'
+    output_path = 'license-recognition/output/'
+    cascade_faces = cv2.CascadeClassifier('license-recognition/models/haarcascade_frontalface_default.xml')
+    for i in range(1, 51):
+        img = cv2.imread(input_path + str(i) + '.jpg')
+        img = resize(img)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = cascade_faces.detectMultiScale(gray, 1.1, 4)
+        # Draw rectangles around faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imwrite(output_path + str(i) + '.jpg', img)
+        text = pytesseract.image_to_string(gray)
+        with open(output_path + str(i) + '.txt', 'w') as f:
+            f.write(text)
 
-# List of available languages
-#print(pytesseract.get_languages(config=''))
-
-# Adding custom options
-#custom_config = r'--oem 3 --psm 6'
-print(pytesseract.image_to_string(gray))
-
-
-# load the input image
-# Converting the image into grayscale
-
-cascade_faces = cv2.CascadeClassifier('license-recognition/models/haarcascade_frontalface_default.xml')
-faces = cascade_faces.detectMultiScale(gray, 1.1, 4)
-# Draw rectangles around faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-# #
-# # # Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
+if __name__ == '__main__':
+    main()
